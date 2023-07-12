d3ad349dfafce0f172ebaf0136260cb5669a0cf3
Javier Navarro <javierna182@gmail.com>
2023-07-11 21:54:31 -0500

    base mode completed

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,13 +1,16 @@
 import{ useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function Review() {
     const store = useSelector(store => store);
     const dispatch = useDispatch();
+    const history = useHistory(); 
 
     const addNewFeedback = (event) => {
         event.preventDefault();
         dispatch({ type: "ADD_FEEDBACK", payload: store });
+        history.push('/completed');
     }
 
     return(
